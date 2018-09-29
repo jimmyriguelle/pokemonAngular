c10a5a65fcaeffc2ab1f5fd24b5b5176ec6c86ca
jimmyriguelle <jimmyriguelle@hotmail.com>
2018-09-29 16:16:43 +0200

    ajout de plusieurs page

---
+++ src/app/add-favorite/add-favorite.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-favorite',
+  templateUrl: './add-favorite.component.html',
+  styleUrls: ['./add-favorite.component.css']
+})
+export class AddFavoriteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { DetailsComponent } from './details/details.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { AddFavoriteComponent } from './add-favorite/add-favorite.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    DetailsComponent,
+    FavoriteComponent,
+    AddFavoriteComponent
   ],
   imports: [
     BrowserModule
